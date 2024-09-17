add2957286e0f149e2ea4a4f5fbf563970155e2e
Parth Mangtani <impmmmm@gmail.com>
2024-09-17 21:59:37 +0530

    :bookmark: update to v1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = "square_database_helper"
 
 setup(
     name=package_name,
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=["requests>=2.31.0"],
     author="thePmSquare",
