4fb44e0cad026a4cea5a11e87af9bf46ab83c29e
Parth Mangtani <impmmmm@gmail.com>
2024-10-31 09:22:03 +0530

    :memo: update example to match new functions

--- example.py
+++ example.py
@@ -6,14 +6,14 @@ square_database_helper = SquareDatabaseHelper()
 insert_data = [
     {"test_text": "example"},
 ]
-insert_output = square_database_helper.insert_rows(
+insert_output = square_database_helper.insert_rows_v0(
     insert_data, "square", "public", "test"
 )
 print(insert_output)
 
 # Example: Get Rows
 get_filters = {}
-get_output = square_database_helper.get_rows(
+get_output = square_database_helper.get_rows_v0(
     get_filters, "square", "public", "test", ignore_filters_and_get_all=True, limit=1
 )
 print(get_output)
@@ -21,14 +21,14 @@ print(get_output)
 # Example: Edit Rows
 edit_data = {"test_text": "edited"}
 edit_filters = {"test_text": "example"}
-edit_output = square_database_helper.edit_rows(
+edit_output = square_database_helper.edit_rows_v0(
     edit_data, edit_filters, "square", "public", "test"
 )
 print(edit_output)
 
 # Example: Delete Rows
 delete_filters = {"test_text": "edited"}
-delete_output = square_database_helper.delete_rows(
+delete_output = square_database_helper.delete_rows_v0(
     delete_filters, "square", "public", "test"
 )
 print(delete_output)
